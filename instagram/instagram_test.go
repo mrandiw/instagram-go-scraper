7017e27d4beb2a8d43e93c74780577ec6ae4a5a7
Andi Wibowo <andi.w@archi.email>
2024-08-18 11:00:57 +0700

    fix unit test

--- instagram/instagram_test.go
+++ instagram/instagram_test.go
@@ -1,7 +1,6 @@
 package instagram
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -15,7 +14,6 @@ func TestGetFullBio(t *testing.T) {
 
 	// Call GetFullBio with the test username
 	profile := insta.GetFullBio("ariescarlas")
-	fmt.Println(profile)
 
 	// Validate the scraped data
 	expectedProfile := InstagramProfile{
@@ -24,7 +22,7 @@ func TestGetFullBio(t *testing.T) {
 		Followers:    5,
 		Following:    268,
 		Post:         1,
-		PhotoProfile: "https://scontent.cdninstagram.com/v/t51.2885-19/162875510_451501566051191_8601471211802227301_n.jpg?stp=dst-jpg_p100x100&_nc_cat=110&ccb=1-7&_nc_sid=fcb8ef&_nc_ohc=eHGO8MFoAGoQ7kNvgHAdjfi&_nc_ht=scontent.cdninstagram.com&oh=00_AYB8qNnkq1oZAtD5NHkKDDR6VGW67WM4VBB6eG8bA0XwnA&oe=66C728C1",
+		PhotoProfile: "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-19/162875510_451501566051191_8601471211802227301_n.jpg?stp=dst-jpg_p100x100&_nc_cat=110&ccb=1-7&_nc_sid=fcb8ef&_nc_ohc=eHGO8MFoAGoQ7kNvgHAdjfi&_nc_ht=scontent.cdninstagram.com&oh=00_AYB8qNnkq1oZAtD5NHkKDDR6VGW67WM4VBB6eG8bA0XwnA&oe=66C728C1",
 	}
 
 	if profile != expectedProfile {
